fab150a00e9217a9a30021137aa1d298f57b2d04
shummi7 <sharmila16moorthi@gmail.com>
2019-02-04 14:25:40 -0800

    Updates

---
+++ precache-manifest.0bfcc962d937a0ed2c97fb34107f78f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/testing/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "47854e5355f3c459be53",
+    "url": "/testing/static/js/runtime~main.47854e53.js"
+  },
+  {
+    "revision": "be028e63d020a2e58594",
+    "url": "/testing/static/js/main.be028e63.chunk.js"
+  },
+  {
+    "revision": "37f62de3e2e43e2994bc",
+    "url": "/testing/static/js/1.37f62de3.chunk.js"
+  },
+  {
+    "revision": "be028e63d020a2e58594",
+    "url": "/testing/static/css/main.54d73546.chunk.css"
+  },
+  {
+    "revision": "37f62de3e2e43e2994bc",
+    "url": "/testing/static/css/1.1966a08f.chunk.css"
+  },
+  {
+    "revision": "37d3cf0bfa6517395bb9ac7e69fa7432",
+    "url": "/testing/index.html"
+  }
+];
